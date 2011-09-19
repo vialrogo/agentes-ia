7f07de97b144dc8477d36b1a82d66c00adced5f9
marvinesv@gmail.com <marvinesv@gmail.com@ca887e4e-6a9c-ba0a-296a-67c7ef400be3>
2011-09-19 21:33:36 +0000

    Implementado Amplitud Corregido el arreglo de dimensiones de Algoritmos Y en nodo está la función imprimir, para depurar

--- amplitud.cpp
+++ amplitud.cpp
@@ -1,5 +1,76 @@
 #include "amplitud.h"
 
-Amplitud::Amplitud()
+Amplitud::Amplitud(bool *dirIn) : Algoritmos(dirIn)
 {
+    direcciones= new bool[7]; //true para vertical, false para horizontal
+    for(int i=0; i<7; i++) direcciones[i]=dirIn[i];
 }
+
+//string Amplitud::buscarSolucion(Nodo *estadoInicial)
+//{
+//    Nodo *actual=estadoInicial;
+//    QQueue<Nodo*> *miCola= new QQueue<Nodo*>();
+//    list<Nodo*> *listaTmp=new list<Nodo*>();
+
+//    listaTmp =expandir(actual);
+//    while(listaTmp!=0)
+//    {
+//        while(!listaTmp->empty())
+//        {
+////            listaTmp->front()->imprimir();
+//            if(!(listaTmp->front()== actual->getPadre()))
+//                miCola->enqueue(listaTmp->front());
+//            listaTmp->pop_front();
+//        }
+//        actual=miCola->dequeue();
+//        listaTmp=0;
+//        listaTmp =expandir(actual);
+//    }
+
+//    return actual->getOperadorAplicado(); //Ventana se encarga de traducir ese string :P
+//}
+
+//int main(){
+//    Amplitud *ensayo;
+
+//    char** prueba=new char*[7];
+//    char** resPrueba=new char*[7];
+//    for (int var = 0; var < 7; var++) {
+//        prueba[var]=new char[7];
+//        resPrueba[var]=new char[7];
+//    }
+////    prueba[0][0]='E';prueba[0][1]='E';prueba[0][2]='E';prueba[0][3]='0';prueba[0][4]='1';prueba[0][5]='1';prueba[0][6]='1';
+////    prueba[1][0]='0';prueba[1][1]='0';prueba[1][2]='0';prueba[1][3]='0';prueba[1][4]='0';prueba[1][5]='1';prueba[1][6]='0';
+////    prueba[2][0]='A';prueba[2][1]='A';prueba[2][2]='C';prueba[2][3]='0';prueba[2][4]='0';prueba[2][5]='0';prueba[2][6]='D';
+////    prueba[3][0]='1';prueba[3][1]='0';prueba[3][2]='C';prueba[3][3]='0';prueba[3][4]='0';prueba[3][5]='0';prueba[3][6]='D';
+////    prueba[4][0]='F';prueba[4][1]='1';prueba[4][2]='G';prueba[4][3]='G';prueba[4][4]='G';prueba[4][5]='B';prueba[4][6]='0';
+////    prueba[5][0]='F';prueba[5][1]='0';prueba[5][2]='0';prueba[5][3]='0';prueba[5][4]='0';prueba[5][5]='B';prueba[5][6]='1';
+////    prueba[6][0]='F';prueba[6][1]='0';prueba[6][2]='0';prueba[6][3]='1';prueba[6][4]='0';prueba[6][5]='1';prueba[6][6]='1';
+//    prueba[0][0]='E';prueba[0][1]='E';prueba[0][2]='E';prueba[0][3]='0';prueba[0][4]='0';prueba[0][5]='0';prueba[0][6]='1';
+//    prueba[1][0]='0';prueba[1][1]='0';prueba[1][2]='0';prueba[1][3]='0';prueba[1][4]='0';prueba[1][5]='0';prueba[1][6]='0';
+//    prueba[2][0]='A';prueba[2][1]='A';prueba[2][2]='C';prueba[2][3]='0';prueba[2][4]='0';prueba[2][5]='0';prueba[2][6]='D';
+//    prueba[3][0]='0';prueba[3][1]='0';prueba[3][2]='C';prueba[3][3]='0';prueba[3][4]='0';prueba[3][5]='0';prueba[3][6]='D';
+//    prueba[4][0]='F';prueba[4][1]='0';prueba[4][2]='G';prueba[4][3]='G';prueba[4][4]='G';prueba[4][5]='B';prueba[4][6]='0';
+//    prueba[5][0]='F';prueba[5][1]='0';prueba[5][2]='0';prueba[5][3]='0';prueba[5][4]='0';prueba[5][5]='B';prueba[5][6]='0';
+//    prueba[6][0]='F';prueba[6][1]='0';prueba[6][2]='0';prueba[6][3]='1';prueba[6][4]='0';prueba[6][5]='0';prueba[6][6]='0';
+
+//    bool *pruebaDir=new bool[6];
+//    pruebaDir[0]=false;
+//    pruebaDir[1]=true;
+//    pruebaDir[2]=true;
+//    pruebaDir[3]=true;
+//    pruebaDir[4]=false;
+//    pruebaDir[5]=true;
+//    pruebaDir[6]=false;
+
+//    Nodo *inicio= new Nodo(prueba, 0, "", 0, 0);
+//    ensayo=new Amplitud(pruebaDir);
+//    string sol=ensayo->buscarSolucion(inicio);
+//    cout << sol << endl;
+////    list<Nodo*>* miLista=ensayo->expandir(inicio);
+////    while(!miLista->empty()){
+////        ensayo->imprimir(miLista->front()->getEstado());
+////        miLista->pop_front();
+////        cout<<endl;
+////    }
+//}
--- amplitud.h
+++ amplitud.h
@@ -1,12 +1,16 @@
 #ifndef AMPLITUD_H
 #define AMPLITUD_H
 
+#include <QQueue>
 #include "algoritmos.h"
 
 class Amplitud : public Algoritmos
 {
+private:
+    bool *direcciones;
 public:
-    Amplitud();
+    Amplitud(bool *dirIn);
+    string buscarSolucion(Nodo* estadoInicial);
 };
 
 #endif // AMPLITUD_H
