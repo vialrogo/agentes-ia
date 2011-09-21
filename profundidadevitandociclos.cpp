ea025717d51e41d6a673b0a237b73d46f1f5c36b
marvinesv@gmail.com <marvinesv@gmail.com@ca887e4e-6a9c-ba0a-296a-67c7ef400be3>
2011-09-21 19:26:13 +0000

    Implementado profundidad, pero falta probarlo

--- profundidadevitandociclos.cpp
+++ profundidadevitandociclos.cpp
@@ -1,5 +1,41 @@
 #include "profundidadevitandociclos.h"
 
-ProfundidadEvitandoCiclos::ProfundidadEvitandoCiclos()
+ProfundidadEvitandoCiclos::ProfundidadEvitandoCiclos(bool *dirIn)
 {
+    direcciones= new bool[7]; //true para vertical, false para horizontal
+    for(int i=0; i<7; i++) direcciones[i]=dirIn[i];
+}
+
+string ProfundidadEvitandoCiclos::buscarSolucion(Nodo* estadoInicial)
+{
+    Nodo *actual=estadoInicial;
+    QStack <Nodo*> *miPila= new QStack<Nodo*>();
+    list<Nodo*> *listaTmp=new list<Nodo*>();
+
+    listaTmp=expandir(actual);
+    while(listaTmp!=0)
+    {
+        while(!listaTmp->empty())
+        {
+            if(!generaCiclo(listaTmp->back()))
+                miPila->push(listaTmp->back());
+            listaTmp->pop_back();
+        }
+        actual=miPila->pop();
+        listaTmp=0;
+        listaTmp=expandir(actual);
+    }
+    return actual->getOperadorAplicado(); //Ventana se encarga de traducir ese string :P
+}
+
+bool ProfundidadEvitandoCiclos::generaCiclo(Nodo *nodito)
+{
+    bool cicloHallado=false;
+    Nodo *tmp=nodito->getPadre();
+    while(tmp!=0)
+    {
+        if(nodito==tmp) cicloHallado=true;
+        tmp=tmp->getPadre();
+    }
+    return cicloHallado;
 }
--- profundidadevitandociclos.h
+++ profundidadevitandociclos.h
@@ -1,12 +1,17 @@
 #ifndef PROFUNDIDADEVITANDOCICLOS_H
 #define PROFUNDIDADEVITANDOCICLOS_H
 
+#include <QStack>
 #include "algoritmos.h"
 
 class ProfundidadEvitandoCiclos : public Algoritmos
 {
+private:
+    bool *direcciones;
 public:
-    ProfundidadEvitandoCiclos();
+    ProfundidadEvitandoCiclos(bool *dirIn);
+    string buscarSolucion(Nodo* estadoInicial);
+    bool generaCiclo(Nodo *nodito);
 };
 
 #endif // PROFUNDIDADEVITANDOCICLOS_H
