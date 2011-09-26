667372356e6df1cd0a63aebf9da2c583b2df28cc
vialrogo@gmail.com <vialrogo@gmail.com@ca887e4e-6a9c-ba0a-296a-67c7ef400be3>
2011-09-26 03:21:23 +0000

    Se eliminó la clase priorityqueue porque se ahora se implementa con la clase nativa de c++. Costo uniforme funciona parcialmente, al parecer es muy costoso tener una cola de prioridad y con ciertos mapas se muere, creo uqe tiene que ver directamtne con el factor de ramificación :-/

--- trunk/costouniforme.h
+++ trunk/costouniforme.h
@@ -2,7 +2,7 @@
 #define COSTOUNIFORME_H
 
 #include "algoritmos.h"
-#include "priorityqueue.cpp"
+#include <queue>
 
 class CostoUniforme : public Algoritmos
 {
@@ -12,5 +12,19 @@ public:
     CostoUniforme(bool *dirIn);
     string buscarSolucion(Nodo* estadoInicial);
 };
+
+class mycomparison
+{
+  bool reverse;
+public:
+  mycomparison(const bool& revparam=false)
+    {reverse=revparam;}
+  bool operator() (Nodo* lh, Nodo* rhs) const
+  {
+    if (reverse) return (lh->getCosto()>rhs->getCosto());
+    else return (lh->getCosto()<rhs->getCosto());
+  }
+};
+
 
 #endif // COSTOUNIFORME_H
