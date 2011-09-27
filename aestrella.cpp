ff77534083da902b46e776c59e19c364f8f7917e
vialrogo@gmail.com <vialrogo@gmail.com@ca887e4e-6a9c-ba0a-296a-67c7ef400be3>
2011-09-27 17:05:23 +0000

    Avara y A* supuestamente funcionando, toca probar

--- aestrella.cpp
+++ aestrella.cpp
@@ -1,5 +1,36 @@
 #include "aestrella.h"
 
-AEstrella::AEstrella()
+AEstrella::AEstrella(bool *dirIn) : Algoritmos(dirIn)
 {
+    direcciones= new bool[7]; //true para vertical, false para horizontal
+    for(int i=0; i<7; i++) direcciones[i]=dirIn[i];
+}
+
+string AEstrella::buscarSolucion(Nodo *estadoInicial)
+{
+    Nodo *actual=estadoInicial;
+    set.insert(QString::fromStdString(actual->getEstadoString()));
+    priority_queue<Nodo*,vector<Nodo*>,comparaHeuristicasCosto> *miCola = new priority_queue<Nodo*,vector<Nodo*>,comparaHeuristicasCosto>(comparaHeuristicasCosto(true));
+    list<Nodo*> *listaTmp=new list<Nodo*>();
+
+    listaTmp =expandir(actual);
+    while(listaTmp!=0)
+    {
+        while(!listaTmp->empty())
+        {
+            if(!set.contains(QString::fromStdString(listaTmp->front()->getEstadoString())))
+            {
+                miCola->push(listaTmp->front());
+                set.insert(QString::fromStdString(listaTmp->front()->getEstadoString()));
+            }
+            listaTmp->pop_front();
+        }
+        actual=miCola->top();
+        miCola->pop();
+//        cout<<miCola->size()<<endl;
+        listaTmp=0;
+        listaTmp =expandir(actual);
+    }
+
+    return actual->getOperadorAplicado();
 }
--- aestrella.h
+++ aestrella.h
@@ -2,11 +2,35 @@
 #define AESTRELLA_H
 
 #include "algoritmos.h"
+#include <queue>
+#include <QSet>
 
 class AEstrella : public Algoritmos
 {
+private:
+    bool *direcciones;
+    QSet<QString> set;
 public:
-    AEstrella();
+    AEstrella(bool *dirIn);
+    string buscarSolucion(Nodo* estadoInicial);
+};
+
+class comparaHeuristicasCosto
+{
+private:
+    bool reverse;
+
+public:
+    comparaHeuristicasCosto(const bool& revparam=false)
+    {
+        reverse=revparam;
+    }
+
+    bool operator() (Nodo* lh, Nodo* rhs) const
+    {
+        if (reverse) return ( (lh->getHeuristica()+lh->getCosto()) > (rhs->getHeuristica()+rhs->getCosto()) );
+        else return ( (lh->getHeuristica()+lh->getCosto()) < (rhs->getHeuristica()+rhs->getCosto()) );
+    }
 };
 
 #endif // AESTRELLA_H
