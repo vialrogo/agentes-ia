bf709d330e62a5af7b780d6396ed13f038e3b7e2
vialrogo@gmail.com <vialrogo@gmail.com@ca887e4e-6a9c-ba0a-296a-67c7ef400be3>
2011-10-13 23:10:10 +0000

    Hilo funcionando

--- agente.h
+++ agente.h
@@ -1,13 +1,14 @@
 #ifndef AGENTE_H
 #define AGENTE_H
 
+#include <QThread>
 #include "amplitud.h"
 #include "costouniforme.h"
 #include "profundidadevitandociclos.h"
 #include "avara.h"
 #include "aestrella.h"
 
-class Agente
+class Agente : public QThread
 {
 private:
     bool *direcciones;
@@ -16,15 +17,18 @@ private:
     ProfundidadEvitandoCiclos *ptrProfundidadEvitandoCiclos;
     Avara *ptrAvara;
     AEstrella *ptrAEstrella;
+    int cual; //0=> amplitud, 1=>costo, 2=>profundidad, 3=>Avara, 4=>A-estrella
+    char** matrizInicial;
+    string solucion;
+
 public:
     Agente();
     ~Agente();
     void setDireciones(bool *dirIn);
-    string BuscarAmplitud(char **matInicial);
-    string BuscarCostoUniforme(char **matInicial);
-    string BuscarProfundidadEvitandoCiclos(char **matInicial);
-    string BuscarAvara(char **matInicial);
-    string BuscarAEstrella(char **matInicial);
+    void run();
+    void setCual(int cual_in);
+    void serMatrizInicial(char** matrizInicial_in);
+    string getSolucion();
 };
 
 #endif // AGENTE_H
